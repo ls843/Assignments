144b553ef54c14d3a8d58517c5527f77d70868a3
ls843 <Laxman.G@bounteous.com>
2026-03-04 03:21:34 -0800

    assignments

---
+++ springSecurityWithJWT/src/test/java/org/example/springsecuritywithjwt/SpringSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecuritywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
